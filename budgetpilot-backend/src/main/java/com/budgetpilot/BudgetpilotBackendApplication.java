0262b1204e7ecdd93fae4f1cf709a031e0d3d5b1
Rayen Benassi <Rayen.Benassi+fnzgroup@fnz.com>
2026-08-18 07:53:25 +0100

    Initialize Spring Boot backend

---
+++ budgetpilot-backend/src/main/java/com/budgetpilot/BudgetpilotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.budgetpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetpilotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetpilotBackendApplication.class, args);
+	}
+
+}
---
+++ budgetpilot-backend/src/main/java/com/budgetpilot/health/HealthController.java
@@ -0,0 +1,13 @@
+package com.budgetpilot.health;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("api/health")
+    public String health(){
+        return "BudgetPilot backend is running";
+    }
+}
